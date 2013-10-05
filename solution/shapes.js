faa42ee140447d6fb1880c692a4e966a895fbd1f
Tiangang Liu <liutg@hotmail.com>
2013-10-04 23:30:05 -0700

    Rectangle

--- solution/shapes.js
+++ solution/shapes.js
@@ -81,21 +81,55 @@ function Line() {
 		return this;
 	};
 }
-// 
+// Object Rectangle
 function Rectangle() {
-	this.color = red;
-	this.cX = 0;
-	this.cY = 0;
-	this.height = 0;
+	this._color = "red";
+	this._cX = 0;
+	this._cY = 0;
+	this._height = 0;
 	this._width = 0;
 	
 	this.center = function(c1, c2) {
-		this.cX = c1;
-		this.cY = c2;
+		this._cX = c1;
+		this._cY = c2;
 		return this;
 	};
 	this.width = function() {
-		
+		var numargs = arguments.length;
+		if (numargs == 0)
+			return this._width;
+		else {
+			this._width = arguments[0];
+			return this;
+		}
 	};
-	
+	this.height = function() {
+		var numargs = arguments.length;
+		if (numargs == 0)
+			return this._height;
+		else {
+			this._height = arguments[0];
+			return this;
+		}
+	};
+	this.fill = function() {
+		var numargs = arguments.length;
+		if (numargs == 0)
+			return this._color;
+		else {
+			this._color = arguments[0];
+			return this;
+		}
+	};
+	this.draw = function(svg) {
+		var shape = document.createElementNS(NS, "rect");
+    	shape.setAttributeNS(null, "x", this._cX - this._width/2);
+    	shape.setAttributeNS(null, "y", this._cY - this._height/2);
+    	shape.setAttributeNS(null, "height", this._height);
+    	shape.setAttributeNS(null, "width", this._width);
+    	shape.setAttributeNS(null, "fill", this._color);
+
+		svg.appendChild(shape);
+		return this;
+	};
 }
