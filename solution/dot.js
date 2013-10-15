583ff42fd95b0fa9e4e6adefa1570a958b2a4830
Tiangang Liu <liutg@hotmail.com>
2013-10-14 17:30:03 -0700

    dot 1st working ver

--- solution/dot.js
+++ solution/dot.js
@@ -2,40 +2,64 @@ $(function () {
     // Grab the data
     var data = [],
         axisx = [],
-        axisy = [],
-        table = $("#for-chart");
-    $("tbody td", table).each(function (i) {
-        data.push(parseFloat($(this).text(), 10));
-    });
-    table.hide();
-    $("tbody th", table).each(function () {
-        axisy.push($(this).text());
-    });
-    $("tfoot th", table).each(function () {
-        axisx.push($(this).text());
-    });
+        axisy = [];
+        
+    var algorithm = {};
+	var system = {};
+	var dataType = {};
+	var runtimeData = {};
+
+	for (var i in runtime){
+		var rt = runtime[i];
+		algorithm[rt.algorithm] = true;
+		system[rt.system] = true;
+		dataType[rt.type] = true;
+		var idx = rt.algorithm + rt.system + rt.type;
+		runtimeData[idx] = rt.time;
+	}
+	// Axis Y -- System
+	for (var s in system)  {
+		axisy.push(s);
+	}
+	// Axis X -- Type
+	for (var d in dataType) {
+		axisx.push(d);
+	}
+	
+	// Data -- runtime for each algorithm
+	var alg_idx = 0;
+	for (var a in algorithm) {
+		data[alg_idx] = new Array();
+		for (var s in system) {
+			for (var d in dataType) {
+				var idx = a + s + d;
+				data[alg_idx].push(runtimeData[idx]);
+			}
+		}
+		alg_idx++;
+	}
     // Draw
     var width = 800,
         height = 300,
         leftgutter = 30,
         bottomgutter = 20,
         r = Raphael("chart", width, height),
-        txt = {"font": '10px Fontin-Sans, Arial', stroke: "none", fill: "#fff"},
+        txt = {"font": '18px Fontin-Sans, Arial', stroke: "none", fill: "#fff"},
         X = (width - leftgutter) / axisx.length,
         Y = (height - bottomgutter) / axisy.length,
         color = $("#chart").css("color");
         max = Math.round(X / 2) - 1;
     // r.rect(0, 0, width, height, 5).attr({fill: "#000", stroke: "none"});
     for (var i = 0, ii = axisx.length; i < ii; i++) {
-        r.text(leftgutter + X * (i + .5), 294, axisx[i]).attr(txt);
+        r.text(leftgutter + X * (i + .5), 280, axisx[i]).attr(txt);
     }
     for (var i = 0, ii = axisy.length; i < ii; i++) {
-        r.text(10, Y * (i + .5), axisy[i]).attr(txt);
+        r.text(30, Y * (i + .5), axisy[i]).attr(txt);
     }
     var o = 0;
     for (var i = 0, ii = axisy.length; i < ii; i++) {
         for (var j = 0, jj = axisx.length; j < jj; j++) {
-            var R = data[o] && Math.min(Math.round(Math.sqrt(data[o] / Math.PI) * 4), max);
+            var R = data[0][o] && Math.min(Math.round(Math.sqrt(data[0][o] / Math.PI) * 4), max)*5;
             if (R) {
                 (function (dx, dy, R, value) {
                     var color = "hsb(" + [(1 - R / max) * .5, 1, .75] + ")";
@@ -43,7 +67,7 @@ $(function () {
                     if (R < 6) {
                         var bg = r.circle(dx + 60 + R, dy + 10, 6).attr({stroke: "none", fill: "#000", opacity: .4}).hide();
                     }
-                    var lbl = r.text(dx + 60 + R, dy + 10, data[o])
+                    var lbl = r.text(dx + 60 + R, dy + 10, data[0][o])
                             .attr({"font": '10px Fontin-Sans, Arial', stroke: "none", fill: "#fff"}).hide();
                     var dot = r.circle(dx + 60 + R, dy + 10, max).attr({stroke: "none", fill: "#000", opacity: 0});
                     dot[0].onmouseover = function () {
@@ -64,7 +88,7 @@ $(function () {
                         }
                         lbl.hide();
                     };
-                })(leftgutter + X * (j + .5) - 60 - R, Y * (i + .5) - 10, R, data[o]);
+                })(leftgutter + X * (j + .5) - 60 - R, Y * (i + .5) - 10, R, data[0][o]);
             }
             o++;
         }
